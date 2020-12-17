81ed19e37511ca1ab097053c6d63062733512fd9
milenamarchese <mmarchese@fi.uba.ar>
2020-12-17 00:46:36 -0300

    documentation

--- TP2/cache.h
+++ TP2/cache.h
@@ -22,25 +22,61 @@ typedef struct cache {
 	int last_hit;
 } cache_t;
 
-
+/*
+ * Pre: The cache global variable fields: n_ways, cache_size and block_size must
+ * be initialized.
+ * Initializes the cache from the cache global variable.
+ * Sets as invalid, the simulated memory at 0, and the misses rate at 0.
+ */
 void init();
 
+/*
+* Receives an address and returns the cache set that belongs to that address.
+*/
 unsigned int find_set(int address);
 
+/*
+* Receives a set number and returns the index of a free block of the set.
+* If there is no free block returns the least recently used block.
+*/
 unsigned int find_lru(int setnum);
 
+/*
+* Receives a set and an way and returns the state of the D bit.
+*/
 unsigned int is_dirty(int way, int setnum);
 
+/*
+* Receives a block number and stores the content from main memory to the corresponding
+* cache block.
+*/
 void read_block(int blocknum);
 
+/*
+* Receives a set and an way and writes the content of the block to main memory.
+*/
 void write_block(int way, int setnum);
 
+/*
+* Receives an address of main memory and if it is necessary writes to the cache.
+* Then proceeds to read from cache.
+*/
 char read_byte(int address);
 
+/*
+* Receives an address of main memory and if it is necessary writes to the cache.
+* Then proceeds to write the block in cache and sets the D bit to dirty.
+*/
 void write_byte(int address, char value);
 
+/*
+* Returns percentage of miss rate.
+*/
 int get_miss_rate();
 
+/*
+* Destroys cache.
+*/
 void cache_destroy();
 
 #endif
