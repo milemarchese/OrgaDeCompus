8d730a69da0c0a29e35b80daa1c285a62e8d483c
JDSanto <javidisanto@gmail.com>
2020-12-16 23:53:11 -0300

    Cambio en is_dirty

--- TP2/test/pruebas.sh
+++ TP2/test/pruebas.sh
@@ -20,7 +20,32 @@ for x in *_in; do
 
     (cat ${b}_in | $PROGRAMA ${args} > out_tmp 2> err_tmp; \
         python3 csvdiff.py ${b}_out out_tmp && \
-        printf "OK\n") || { RET=$?; echo "ERROR"; }
+        echo "OK") || { RET=$?; echo "ERROR"; }
+
+
+done
+
+
+echo "Ejecución de pruebas con valgrind:"
+for x in *_in; do
+    b=${x%_in}
+    printf "${b}... "
+
+    cachesize=$(cat ${b}_in | cut -d$'\n' -f1)
+    nways=$(cat ${b}_in | cut -d$'\n' -f2)
+    blocksize=$(cat ${b}_in | cut -d$'\n' -f3)
+    cmdfile=$(cat ${b}_in | cut -d$'\n' -f4)
+
+    args="-cs ${cachesize} -w ${nways} -bs ${blocksize} ${cmdfile}"
+
+
+    valgrind --quiet --leak-check=full --track-origins=yes --error-exitcode=2 \
+        cat ${b}_in | $PROGRAMA ${args} > out_tmp 2> err_tmp; >/dev/null && \
+        echo "OK" || { RET=$?; echo "ERROR"; }
+
+    # (cat ${b}_in | $PROGRAMA ${args} > out_tmp 2> err_tmp; \
+    #     python3 csvdiff.py ${b}_out out_tmp && \
+    #     printf "OK\n") || { RET=$?; echo "ERROR"; }
 
 
 done
